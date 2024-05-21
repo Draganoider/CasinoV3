929647ded3b95e8cb8d839504c000f382207e862
Draganoider <evgen.draga@gmail.com>
2024-05-21 00:01:36 -0700

    Add project files.

---
+++ CasinoV3/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CasinoV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.comboBoxBet = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SPIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 70);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(328, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 70);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(456, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 70);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(200, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 70);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(328, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 70);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(456, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 70);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(200, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 70);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(328, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 70);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(456, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(122, 70);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBalance.Location = new System.Drawing.Point(12, 347);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(205, 32);
+            this.labelBalance.TabIndex = 10;
+            this.labelBalance.Text = "1000$";
+            this.labelBalance.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxBet
+            // 
+            this.comboBoxBet.FormattingEnabled = true;
+            this.comboBoxBet.Items.AddRange(new object[] {
+            "0.25$",
+            "0.50$",
+            "1.00$",
+            "2.00$",
+            "5.00$",
+            "10.00$",
+            "15.00$",
+            "20.00$",
+            "30.00$",
+            "50.00$"});
+            this.comboBoxBet.Location = new System.Drawing.Point(596, 355);
+            this.comboBoxBet.Name = "comboBoxBet";
+            this.comboBoxBet.Size = new System.Drawing.Size(114, 24);
+            this.comboBoxBet.TabIndex = 11;
+            this.comboBoxBet.Text = "Bet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 456);
+            this.Controls.Add(this.comboBoxBet);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.ComboBox comboBoxBet;
+    }
+}
+
---
+++ CasinoV3/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CasinoV3
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private string[] symbols = { "🍒", "🍋", "🍉", "🍇", "🔔" };
+        private Label[,] symbolLabels = new Label[3, 3];
+        private bool spinning = false;
+        private int tickCount = 0;
+        private int stopAfterTicks = 7;
+
+
+        private double balance = 1000.00;
+        private double bet = 0.00;
+        private double multiplier = 0.00;
+        private double winAmount = 0.00;
+
+
+
+        //rtp section
+        private int rtp = 95;
+        private bool needWin = false;
+        private int spinCounter = 0;
+        private double pastBalance = 1000.00;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            UpdateBalanceDisplay();
+
+            InitializeSymbolLabels();
+            timer1.Interval = 100;
+            timer1.Tick += timer1_Tick;
+        }
+
+        private void InitializeSymbolLabels()
+        {
+            int labelIndex = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    string labelName = $"label{labelIndex++}";
+                    symbolLabels[i, j] = this.Controls.Find(labelName, true).FirstOrDefault() as Label;
+                    if (symbolLabels[i, j] == null)
+                    {
+                        MessageBox.Show($"Label not found: {labelName}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tickCount < stopAfterTicks)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        symbolLabels[i, j].Text = symbols[random.Next(symbols.Length)];
+                    }
+                }
+                tickCount++;
+            }
+            else if (needWin)
+            {
+                //spin variation
+                /*
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        symbolLabels[i, j].Text = symbols[random.Next(symbols.Length)];
+
+
+                        bool allSame = symbolLabels.Cast<Label>().Where((label, index) => index >= 3 && index < 6)
+                        .Select(label => label.Text)
+                        .Distinct()
+                        .Count() == 1;
+
+                        if (allSame)
+                        {
+                            needWin = false;
+                            break;
+                        }
+
+                    }
+                
+
+                }
+                
+                 */
+
+                int randomSymbol = random.Next(symbols.Length);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i == 1)
+                        {
+                            symbolLabels[i, j].Text = symbols[randomSymbol];
+                        }
+                        else
+                        {
+                            symbolLabels[i, j].Text = symbols[random.Next(symbols.Length)];
+                        }
+                        
+                    }
+                }
+                needWin = false;
+                pastBalance = balance;
+
+                //tickCount++;
+            }
+            else
+            {
+                timer1.Stop();
+                spinning = false;
+                CheckWin();
+            }
+        }
+
+        private void CheckWin()
+        {
+            bool allSame = symbolLabels.Cast<Label>().Where((label, index) => index >= 3 && index < 6)
+                .Select(label => label.Text)
+                .Distinct()
+                .Count() == 1;
+
+            if (allSame)
+            {
+                string uniqueSymbol = symbolLabels.Cast<Label>().Where((label, index) => index >= 3 && index < 6)
+                .Select(label => label.Text)
+                .Distinct()
+                .First();
+
+                switch (uniqueSymbol)
+                {
+                    case "🍉": 
+                        multiplier = 3.00; 
+                        break;
+
+                    case "🍒":
+                        multiplier = 5.00;
+                        break;
+
+                    case "🍇":
+                        multiplier = 10.00;
+                        break;
+
+                    case "🍋":
+                        multiplier = 15.00;
+                        break;
+
+                    case "🔔":
+                        multiplier = 30.00;
+                        break;
+                }
+
+                winAmount = bet * multiplier;
+
+                balance = winAmount + balance;
+                UpdateBalanceDisplay();
+
+                MessageBox.Show($"Win!\n{winAmount}$");
+            }
+
+        }
+
+
+
+        private void UpdateBalanceDisplay()
+        {
+            labelBalance.Text = $"Balance: ${balance}";
+        }
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!spinning)
+            {
+                string selectedBet = comboBoxBet.SelectedItem.ToString();
+                selectedBet = selectedBet.Substring(0, selectedBet.Length - 1);
+
+                bet = Convert.ToDouble(selectedBet);
+
+                balance = balance - bet;
+                UpdateBalanceDisplay();
+
+
+                spinning = true;
+                
+                needWin = RtpChecker();
+                tickCount = 0;
+                timer1.Start();
+            }
+            else
+            {
+
+            }
+            
+        }
+
+
+        private bool RtpChecker()
+        {
+            int randomSpin = random.Next(10, 21);
+
+            double balanceNeeded = pastBalance / 100 * rtp; 
+
+            if (balance < balanceNeeded)
+            {
+                spinCounter++;
+            }
+
+            if (spinCounter >= randomSpin && balance < balanceNeeded )
+            {
+                needWin = true;
+                spinCounter = 0;
+            }
+
+            
+            
+            return needWin;
+        }
+
+
+
+
+
+
+    }
+}
